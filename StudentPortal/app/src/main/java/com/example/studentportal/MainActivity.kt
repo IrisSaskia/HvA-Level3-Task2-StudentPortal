251d7b19e5b9a146466df49397a74743f20541d4
IrisSaskia <iris.oostra@hva.nl>
2020-03-22 15:33:17 +0100

    Created the activity switch functionality

--- StudentPortal/app/src/main/java/com/example/studentportal/MainActivity.kt
+++ StudentPortal/app/src/main/java/com/example/studentportal/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.studentportal
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -15,10 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fabAdd.setOnClickListener {
+            onAddClick()
         }
+    }
+
+    private fun onAddClick() {
+        val profileActivityIntent = Intent(this, CreatePortalActivity::class.java)
+        //profileActivityIntent.putExtra(CreatePortalActivity.PROFILE_EXTRA, profile)
+        startActivity(profileActivityIntent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
